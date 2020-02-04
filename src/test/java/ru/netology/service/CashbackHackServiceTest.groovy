3e848184f9c9f3e42919558672d54943aa761ebe
alex <alex92471@gmail.com>
2020-02-04 19:38:19 +0300

    Project prepared

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.groovy
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100IfPurchaseIs900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnNothingIfPurchaseIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+}
